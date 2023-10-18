b586d86a830670495cf18d25c65b316e12f8b4f7
LViniciusk <86896520+LViniciusk@users.noreply.github.com>
2023-10-18 13:33:37 -0300

    Queue fix

--- Queue.h
+++ Queue.h
@@ -11,16 +11,17 @@
  *
  */
 template <typename Type>
-class iterator_queue {
-   private:
-    Node<int>* m_ptr;  // ponteiro para o no atual
-   public:
+class iterator_queue
+{
+private:
+    Node<int> *m_ptr; // ponteiro para o no atual
+public:
     /**
      * @brief Construtor da classe iterator_queue
      *
      * @param ptr
      */
-    iterator_queue(Node<Type>* ptr) { m_ptr = ptr; }
+    iterator_queue(Node<Type> *ptr) { m_ptr = ptr; }
 
     /**
      * @brief Sobrecarga do operador de pré-incremento.
@@ -28,7 +29,8 @@ class iterator_queue {
      *
      * @return iterator& retorna uma referencia para o iterador
      */
-    iterator_queue& operator++() {
+    iterator_queue &operator++()
+    {
         m_ptr = m_ptr->next;
         return *this;
     }
@@ -39,7 +41,8 @@ class iterator_queue {
      *
      * @return iterator retorna o iterador antes de ser incrementado
      */
-    iterator_queue operator++(int) {
+    iterator_queue operator++(int)
+    {
         iterator_queue temp = *this;
         m_ptr = m_ptr->next;
         return temp;
@@ -51,7 +54,7 @@ class iterator_queue {
      *
      * @return Type& retorna uma referencia para o dado armazenado no Node
      */
-    Type& operator*() { return m_ptr->data; }
+    Type &operator*() { return m_ptr->data; }
 
     /**
      * @brief Sobrecarga do operador de igualdade.
@@ -60,7 +63,8 @@ class iterator_queue {
      * @return true se os iteradores apontam para o mesmo nó
      * @return false caso contrário
      */
-    bool operator==(const iterator_queue& other) {
+    bool operator==(const iterator_queue &other)
+    {
         return m_ptr == other.m_ptr;
     }
 
@@ -71,24 +75,27 @@ class iterator_queue {
      * @return true se os iteradores apontam para nós diferentes
      * @return false caso contrário
      */
-    bool operator!=(const iterator_queue& other) {
+    bool operator!=(const iterator_queue &other)
+    {
         return m_ptr != other.m_ptr;
     }
 };
 
 template <typename Type>
-class Queue {
-   private:
-    Node<Type>* m_head;  // ponteiro para o no sentinela
-    Node<Type>* m_tail;  // ponteiro para o ultimo nó da fila
-    unsigned m_size{};   // quantidade de elementos na fila
+class Queue
+{
+private:
+    Node<Type> *m_head; // ponteiro para o no sentinela
+    Node<Type> *m_tail; // ponteiro para o ultimo nó da fila
+    unsigned m_size{};  // quantidade de elementos na fila
 
-   public:
+public:
     /**
      * @brief Construtor da classe Queue. Cria uma fila vazia.
      *
      */
-    Queue() {
+    Queue()
+    {
         m_head = new Node<Type>(Type(), nullptr);
         m_tail = m_head;
         m_size = 0;
@@ -99,10 +106,12 @@ class Queue {
      *
      * @param lst - fila a ser copiada
      */
-    Queue(const Queue& lst) {
+    Queue(const Queue &lst)
+    {
         m_head = new Node<Type>(Type(), nullptr);
-        Node<Type>* aux = lst.m_head;
-        while (aux != nullptr) {
+        Node<Type> *aux = lst.m_head;
+        while (aux != nullptr)
+        {
             push(aux->data);
             aux = aux->next;
         }
@@ -143,12 +152,16 @@ class Queue {
      *
      * @param val - valor a ser inserido
      */
-    void push(const int& val) {
-        Node<Type>* aux = new Node<Type>(val, nullptr);
-        if (m_size > 0) {
+    void push(const int &val)
+    {
+        Node<Type> *aux = new Node<Type>(val, nullptr);
+        if (m_size > 0)
+        {
             m_tail->next = aux;
             m_tail = aux;
-        } else {
+        }
+        else
+        {
             m_tail = m_head = aux;
         }
         m_size++;
@@ -159,8 +172,10 @@ class Queue {
      *
      * @return int&
      */
-    int& front() {
-        if (m_size == 0) throw std::runtime_error("empty queue");
+    int &front()
+    {
+        if (m_size == 0)
+            throw std::runtime_error("empty queue");
         return m_head->data;
     }
 
@@ -169,8 +184,10 @@ class Queue {
      *
      * @return const int&
      */
-    const int& front() const {
-        if (m_size == 0) throw std::runtime_error("empty queue");
+    const int &front() const
+    {
+        if (m_size == 0)
+            throw std::runtime_error("empty queue");
         return m_head->data;
     }
 
@@ -178,56 +195,61 @@ class Queue {
      * @brief Função que remove o primeiro elemento da fila
      *
      */
-    void pop() {
-        Node<Type>* aux = m_head;
-        void pop() {
-            if (m_size == 0) throw std::runtime_error(" empty queue ");
-            Node<Type>* aux = m_head;
-            m_head = aux->next;
-            aux->next = nullptr;
-            delete aux;
-            m_size--;
-        }
+    void pop()
+    {
+        if (m_size == 0)
+            throw std::runtime_error(" empty queue ");
+        Node<Type> *aux = m_head;
+        m_head = aux->next;
+        aux->next = nullptr;
+        delete aux;
+        m_size--;
+    }
 
-        /**
-         * @brief Função que retorna uma referencia para o último elemento da
-         * fila
-         *
-         * @return int&
-         */
-        int& back() {
-            if (m_size == 0) throw std::runtime_error("empty queue");
-            return m_tail->data;
-        }
+    /**
+     * @brief Função que retorna uma referencia para o último elemento da
+     * fila
+     *
+     * @return int&
+     */
+    int &back()
+    {
+        if (m_size == 0)
+            throw std::runtime_error("empty queue");
+        return m_tail->data;
+    }
 
-        /**
-         * @brief Função que retorna uma referencia para o último elemento da
-         * fila
-         *
-         * @return const int&
-         */
-        const int& back() const {
-            if (m_size == 0) throw std::runtime_error("empty queue");
-            return m_tail->data;
-        }
+    /**
+     * @brief Função que retorna uma referencia para o último elemento da
+     * fila
+     *
+     * @return const int&
+     */
+    const int &back() const
+    {
+        if (m_size == 0)
+            throw std::runtime_error("empty queue");
+        return m_tail->data;
+    }
 
-        /**
-         * @brief Função que retorna um iterador que aponta para o primeiro
-         * elemento da fila
-         *
-         * @return iterator_queue<Type>
-         */
-        iterator_queue<Type> begin() { return iterator_queue<Type>(m_head); }
+    /**
+     * @brief Função que retorna um iterador que aponta para o primeiro
+     * elemento da fila
+     *
+     * @return iterator_queue<Type>
+     */
+    iterator_queue<Type> begin() { return iterator_queue<Type>(m_head); }
 
-        /**
-         * @brief Função que retorna um iterador que aponta para o primeiro bit
-         * após o último elemento da fila
-         *
-         * @return iterator_queue<Type>
-         */
-        iterator_queue<Type> end() {
-            return iterator_queue<Type>(m_tail->next);
-        }
-    };
+    /**
+     * @brief Função que retorna um iterador que aponta para o primeiro bit
+     * após o último elemento da fila
+     *
+     * @return iterator_queue<Type>
+     */
+    iterator_queue<Type> end()
+    {
+        return iterator_queue<Type>(m_tail->next);
+    }
+};
 
 #endif
