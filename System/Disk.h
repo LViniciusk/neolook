fee9f4cae02cf8e00298defd02537b43a6e530f8
Júnior Silva <128833139+juniors719@users.noreply.github.com>
2023-11-06 14:12:38 -0300

    Update Disk.h

--- System/Disk.h
+++ System/Disk.h
@@ -1,22 +1,30 @@
+/**
+ * @file Disk.h
+ * @author Júnior Silva (junior.silva@alu.ufc.br) - 554222
+ * @author Linyker Vinicius (botlink2030@alu.ufc.br) - 556280
+ * @brief Classe que representa um Disco do computador.
+ * @version 0.1
+ * @date 06-11-2023
+ *
+ *
+ */
+
 #ifndef DISK_H
 #define DISK_H
 
 #include "../TAD'S/PriorityQueue.h"
 #include "../TAD'S/Queue.h"
+#include "../TAD'S/Vector.h"
 #include "Process.h"
 
-/**
- * @brief Classe que representa o disco do computador.
- *
- */
 class Disk {
    private:
     Process* process{};           // processo que está sendo executado no disco
     Queue<Process*>* queue;       // fila de processos do disco
     PriorityQueue<Process*>* pq;  // fila de processos do disco
-    bool politica;  // politica de escalonamento. 0 - FCFS, 1 - SJF
-    bool busy;      // indica se o disco está ocupado
-    int time;       // tempo de execução do processo atual
+    bool politica;                // politica de escalonamento. 0 - FCFS, 1 - SJF
+    bool busy;                    // indica se o disco está ocupado
+    int time;                     // tempo de execução do processo atual
 
    public:
     /**
@@ -37,77 +45,58 @@ class Disk {
     ~Disk() {
         delete queue;
         delete pq;
-        std::cout << "Disco destruido" << std::endl;
     }
 
-    /**
-     * @brief Seta um processo para ser executado no disco. Se o disco já
-     * estiver ocupado, o processo é adicionado na fila de processos do disco.
-     *
-     * @param p Processo a ser executado no disco
-     */
-    void setProcess(Process* p, const long long& timeSystem) {
+    bool isBusy() const { return busy; }
+
+    void setBusy(bool b) { busy = b; }
+
+    Process* getProcess() const { return process; }
+
+    bool setProcess(Process* p) {
         if (!busy) {
-            p->setInstanteDisco(timeSystem);
-            process = p;
-            busy = true;
-            time = 0;
-            // std::cout << "Time " << timeSystem << " - Processo " <<
-            // p->getId()
-            //           << " inserido no Disco" << std::endl;
+            process = p;  // insere o processo na CPU
+            time = 0;     // reseta o tempo de execução
+            busy = true;  // indica que a CPU está ocupada
+            return true;  // indica que o processo foi enviado para a execução na CPU diretamente
         } else {
             if (politica) {
                 pq->push(p->getDisk(), p);
             } else {
                 queue->push(p);
             }
-            // std::cout << "Time " << timeSystem << " - Processo " <<
-            // p->getId()
-            //           << " inserido na fila do Disco" << std::endl;
+            return false;  // indica que o processo foi enviado para a fila de espera da CPU
         }
     }
 
-    /**
-     * @brief Função que executa o processo no disco. Se o disco estiver
-     * ocupada, verifica se o processo terminou e o retorna. Caso contrário,
-     * verifica se há algum processo na fila de processos e o carrega no disco.
-     *
-     * @return Process*
-     */
-    Process* execute(const long long& timeSystem) {
+    bool loadFromQueue() {
+        if (politica) {
+            if (!pq->empty()) {
+                process = pq->top();
+                pq->pop();
+                busy = true;
+                time = 0;
+                return true;
+            }
+        } else {
+            if (!queue->empty()) {
+                process = queue->front();
+                queue->pop();
+                busy = true;
+                time = 0;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    Process* execute() {
         if (busy) {
             if (time == process->getDisk()) {
-                // std::cout << "Time " << timeSystem << " - Processo "
-                //           << process->getId() << " concluido no Disco"
-                //           << std::endl;
                 busy = false;
                 return process;
             } else {
                 time++;
-            }
-        } else {
-            if (politica) {
-                if (!pq->empty()) {
-                    process = pq->top();
-                    pq->pop();
-                    busy = true;
-                    time = 0;
-                    process->setInstanteDisco(timeSystem);
-                    // std::cout << "Time " << timeSystem << " - Processo "
-                    //           << process->getId()
-                    //           << " inserido na fila do Disco" << std::endl;
-                }
-            } else {
-                if (!queue->empty()) {
-                    process = queue->front();
-                    queue->pop();
-                    busy = true;
-                    time = 0;
-                    process->setInstanteDisco(timeSystem);
-                    // std::cout << "Time " << timeSystem << " - Processo "
-                    //           << process->getId()
-                    //           << " inserido na fila do Disco" << std::endl;
-                }
             }
         }
         return nullptr;
