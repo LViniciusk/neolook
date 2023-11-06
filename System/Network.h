01e91f2f11be5e69a41d6aa0a75ee149d4bd71ba
Júnior Silva <128833139+juniors719@users.noreply.github.com>
2023-11-06 14:12:58 -0300

    Update Network.h

--- System/Network.h
+++ System/Network.h
@@ -1,22 +1,30 @@
+/**
+ * @file Network.h
+ * @author Júnior Silva (junior.silva@alu.ufc.br) - 554222
+ * @author Linyker Vinicius (botlink2030@alu.ufc.br) - 556280
+ * @brief Classe que representa a rede do sistema.
+ * @version 0.1
+ * @date 06-11-2023
+ *
+ *
+ */
+
 #ifndef NETWORK_H
 #define NETWORK_H
 
 #include "../TAD'S/PriorityQueue.h"
 #include "../TAD'S/Queue.h"
+#include "../TAD'S/Vector.h"
 #include "Process.h"
 
-/**
- * @brief Classe que representa a rede do sistema.
- *
- */
 class Network {
    private:
     Process* process{};           // processo que está sendo executado no disco
     Queue<Process*>* queue;       // fila de processos do disco
     PriorityQueue<Process*>* pq;  // fila de processos do disco
-    bool politica;  // politica de escalonamento. 0 - FCFS, 1 - SJF
-    bool busy;      // indica se o disco está ocupado
-    int time;       // tempo de execução do processo atual
+    bool politica;                // politica de escalonamento. 0 - FCFS, 1 - SJF
+    bool busy;                    // indica se o disco está ocupado
+    int time;                     // tempo de execução do processo atual
 
    public:
     /**
@@ -36,76 +44,61 @@ class Network {
      * @brief Destroy the Network object
      *
      */
-    ~Network() { std::cout << "Rede destruida" << std::endl; }
+    ~Network() {
+        delete queue;
+        delete pq;
+    }
 
-    /**
-     * @brief Seta um processo para ser executado na rede. Se a rede já
-     * estiver ocupado, o processo é adicionado na fila de processos da rede.
-     *
-     * @param p Processo a ser executado no rede
-     */
-    void setProcess(Process* p, const long long& timeSystem) {
+    bool isBusy() const { return busy; }
+
+    void setBusy(bool b) { busy = b; }
+
+    Process* getProcess() const { return process; }
+
+    bool setProcess(Process* p) {
         if (!busy) {
-            p->setInstanteRede(timeSystem);
-            process = p;
-            busy = true;
-            time = 0;
-            // std::cout << "Time " << timeSystem << " - Processo " <<
-            // p->getId()
-            //           << " inserido na Rede" << std::endl;
+            process = p;  // insere o processo na CPU
+            time = 0;     // reseta o tempo de execução
+            busy = true;  // indica que a CPU está ocupada
+            return true;  // indica que o processo foi enviado para a execução na CPU diretamente
         } else {
             if (politica) {
                 pq->push(p->getNetwork(), p);
             } else {
                 queue->push(p);
             }
-            // std::cout << "Time " << timeSystem << " - Processo " <<
-            // p->getId()
-            //           << " inserido na fila da Rede" << std::endl;
+            return false;  // indica que o processo foi enviado para a fila de espera da CPU
         }
     }
 
-    /**
-     * @brief Função que executa o processo na rede. Se a rede estiver
-     * ocupada, verifica se o processo terminou e o retorna. Caso contrário,
-     * verifica se há algum processo na fila de processos e o carrega na rede.
-     *
-     * @return Process*
-     */
-    Process* execute(const long long& timeSystem) {
+    bool loadFromQueue() {
+        if (politica) {
+            if (!pq->empty()) {
+                process = pq->top();
+                pq->pop();
+                busy = true;
+                time = 0;
+                return true;
+            }
+        } else {
+            if (!queue->empty()) {
+                process = queue->front();
+                queue->pop();
+                busy = true;
+                time = 0;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    Process* execute() {
         if (busy) {
             if (time == process->getNetwork()) {
-                // std::cout << "Time " << timeSystem << " - Processo "
-                //           << process->getId() << " concluido na Rede"
-                //           << std::endl;
                 busy = false;
                 return process;
             } else {
                 time++;
-            }
-        } else {
-            if (politica) {
-                if (!pq->empty()) {
-                    process = pq->top();
-                    pq->pop();
-                    busy = true;
-                    time = 0;
-                    process->setInstanteRede(timeSystem);
-                    // std::cout << "Time " << timeSystem << " - Processo "
-                    //           << process->getId()
-                    //           << " carregado da fila da Rede" << std::endl;
-                }
-            } else {
-                if (!queue->empty()) {
-                    process = queue->front();
-                    queue->pop();
-                    busy = true;
-                    time = 0;
-                    process->setInstanteRede(timeSystem);
-                    // std::cout << "Time " << timeSystem << " - Processo "
-                    //           << process->getId()
-                    //           << " carregado da fila da Rede" << std::endl;
-                }
             }
         }
         return nullptr;
